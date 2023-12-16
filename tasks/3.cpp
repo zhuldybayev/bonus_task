7f16628ecf4eb5742d0ce0d9277c6a908eee6dd1
Azamat <80196565+zhuldybayev@users.noreply.github.com>
2023-12-16 15:45:15 +0600

    Create 3.cpp

---
+++ tasks/3.cpp
@@ -0,0 +1,13 @@
+//palindrome number
+bool isPalindrome(int x) {
+    if (x < 0) {
+        return false;
+    }
+    int number = x;
+    int reverse = 0;
+    while (number) {
+        reverse = reverse * 10 + number % 10;
+        number /= 10;
+    }
+    return x == reverse;
+}
