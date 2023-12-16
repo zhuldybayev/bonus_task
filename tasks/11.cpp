22cf81c57fc8ffbc566cc849231beccb8ef8fe56
Azamat <80196565+zhuldybayev@users.noreply.github.com>
2023-12-16 15:47:56 +0600

    Create 11.cpp

---
+++ tasks/11.cpp
@@ -0,0 +1,22 @@
+//add binary
+class Task {
+ public:
+  string addBinary(string a, string b) {
+    string answer;
+    int carry = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+
+    while (i >= 0 || j >= 0 || carry) {
+      if (i >= 0)
+        carry += a[i--] - '0';
+      if (j >= 0)
+        carry += b[j--] - '0';
+      answer = answer + carry % 2 + '0';
+      carry /= 2;
+    }
+
+    reverse(answer.begin(), answer.end());
+    return answer;
+  }
+};
